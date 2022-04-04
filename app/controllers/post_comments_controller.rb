1619a78bb005102f242a668ccd2a88cc22c5f9b2
EC2 Default User <ec2-user@ip-172-31-19-167.ec2.internal>
2022-04-04 06:16:16 +0000

    [Update]

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,6 +5,7 @@ class PostCommentsController < ApplicationController
     post_comment = PostComment.new(post_comment_params)
     post_comment.user_id = current_user.id
     post_comment.post_image_id = post_image.id
+    post_comment.save
     redirect_to post_image_path(post_image.id)
   end
 
